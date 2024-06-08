44516251edc75869affc87231ec1c9dda5fd9bc4
Sabino Lopes <sabinolopes007@gmail.com>
2024-06-08 00:50:35 -0300

    Cypress - refatorado

--- app/cypress/e2e/spec.cy.js
+++ app/cypress/e2e/spec.cy.js
@@ -1,13 +1,28 @@
+const products = [
+  'Dolce Shine Eau de',
+  'Essence Mascara Lash Princess',
+  'Eyeshadow Palette with Mirror',
+  'Powder Canister',
+  'Red Lipstick',
+  'Red Nail Polish',
+  'Calvin Klein CK One',
+  'Chanel Coco Noir Eau De',
+  'Dior J\'adore',
+  'Gucci Bloom Eau de'
+]
+
+const url = 'http://localhost:3000';
+
 describe('START Store Tests', () => {
   it('Deve ter o primeiro card de produto com o título "Dolce Shine Eau de"', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
     // Verify if the first product card contains the expected title (Dolce Shine Eau de - id 9 )
     cy.get('.card').first().find('h2').should('contain', 'Dolce Shine Eau de');
   });
 
   it('Deve exibir apenas 10 cards de produtos na página', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
     // Verify if there are more than 0 cards on the page and if there are exactly 10 cards
     cy.get('.card').should('have.length.greaterThan', 0);
@@ -23,55 +38,30 @@ describe('START Store Tests', () => {
   });
 
   it('Deve ter o último card de produto com o título "Gucci Bloom Eau de"', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
     // Verify if the last product card contains the expected title (Gucci Bloom Eau de - id 10)
     cy.get('.card').last().find('h2').should('contain', 'Gucci Bloom Eau de');
   });
 
   it('Deve ter o penúltimo card de produto com o título "Dior J\'adore"', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
     // Verify if the second from the last product card contains the expected title (Dior J'adore - id 8)
     cy.get('.card').eq(8).find('h2').should('contain', 'Dior J\'adore');
   });
 
   it('Deve ter cards de produtos na ordem correta', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
-    // Verify if the first card contains the expected title (Dolce Shine Eau de - id 9)
-    cy.get('.card').first().find('h2').should('contain', 'Dolce Shine Eau de');
-
-    // Verify if the second card contains the expected title (Essence Mascara Lash Princess - id 1)
-    cy.get('.card').eq(1).find('h2').should('contain', 'Essence Mascara Lash Princess');
-
-    // Verify if the third card contains the expected title (Eyeshadow Palette with Mirror - id 2)
-    cy.get('.card').eq(2).find('h2').should('contain', 'Eyeshadow Palette with Mirror');
-
-    // Verify if the fourth card contains the expected title (Powder Canister - id 3)
-    cy.get('.card').eq(3).find('h2').should('contain', 'Powder Canister');
-
-    // Verify if the fifth card contains the expected title (Red Lipstick - id 4)
-    cy.get('.card').eq(4).find('h2').should('contain', 'Red Lipstick');
-
-    // Verify if the sixth card contains the expected title (Red Nail Polish - id 5)
-    cy.get('.card').eq(5).find('h2').should('contain', 'Red Nail Polish');
-
-    // Verify if the seventh card contains the expected title (Calvin Klein CK One - id 6)
-    cy.get('.card').eq(6).find('h2').should('contain', 'Calvin Klein CK One');
-
-    // Verify if the eighth card contains the expected title (Chanel Coco Noir Eau De - id 7)
-    cy.get('.card').eq(7).find('h2').should('contain', 'Chanel Coco Noir Eau De');
-
-    // Verify if the ninth card contains the expected title (Dior J'adore - id 8)
-    cy.get('.card').eq(8).find('h2').should('contain', 'Dior J\'adore');
-
-    // Verify if the last card contains the expected title (Gucci Bloom Eau de - id 10)
-    cy.get('.card').last().find('h2').should('contain', 'Gucci Bloom Eau de');
+    cy.get('.card').each((card, index) => {
+      // Verify if the card title is the expected one
+      cy.wrap(card).find('h2').should('contain', products[index]);
+    });
   });
 
   it('Deve navegar até a seção "About Us" quando o link "ABOUT" for clicado', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
     //  Verify if the "ABOUT" link is present and click on it
     cy.contains('ABOUT').click();
@@ -84,7 +74,7 @@ describe('START Store Tests', () => {
   });
 
   it('Deve exibir o rodapé com o ano atual', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(url);
 
     // Verify if the footer is visible
     cy.get('footer').should('be.visible');
